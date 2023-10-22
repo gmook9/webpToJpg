46320864c35a3a3036b748e7a54664b6b7b66b57
gmook9 <gmook9@gmail.com>
2023-10-22 00:58:45 -0700

    clean working directory

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # webpToJpg
-Python scrip to convert webp files to jpg
+Python script to convert webp files to jpg
+
+### Commands setup virtual env with dependencies 
+python -m venv venv
+pip install Pillow 
+venv/Scripts/Activate
+
+### Command to run script
+script.py
--- script.py
+++ script.py
@@ -1,19 +1,15 @@
 import os
 from PIL import Image
 
-# Define input and output directories
-input_folder = "D:/repos/WebpToJPG/input"
-output_folder = "D:/repos/WebpToJPG/output"
+input_folder = "input"
+output_folder = "output"
 
-# Check if the input folder exists and contains WebP files
 if not os.path.exists(input_folder):
     print("Input folder does not exist.")
 else:
-    # Create the output folder if it doesn't exist
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    # List all files in the input directory
     files = os.listdir(input_folder)
     webp_files = [f for f in files if f.lower().endswith(".webp")]
 
@@ -25,10 +21,7 @@ else:
             jpg_filename = os.path.splitext(webp_file)[0] + ".jpg"
             jpg_path = os.path.join(output_folder, jpg_filename)
 
-            # Convert WebP to JPG
             with Image.open(webp_path) as img:
                 img.save(jpg_path, "JPEG")
 
             print(f"Converted: {webp_file} to {jpg_filename}")
-
-print("Conversion complete.")
